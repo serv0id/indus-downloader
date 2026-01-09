ac5ebb4b6aa685694440628f7fb3094eee75d233
serv0id <113516593+serv0id@users.noreply.github.com>
2026-01-09 06:31:03 +0530

    add: IndusDevice class

--- register.py
+++ register.py
@@ -3,20 +3,15 @@ import secrets
 import uuid
 from requests import Session
 import config
+import utils.crypto
 
 
-class Register(object):
+class IndusDevice(object):
     def __init__(self):
-        self.gaid: str
-        self.android_id: str
-        self.device_id: str
-        self.session: Session = Session()
-
-    def register(self) -> None:
-        """
-        Registers a new device with the servers.
-        """
-        pass
+        self.gaid: str = ''
+        self.android_id: str = ''
+        self.device_id: str = ''
+        self.fingerprint: str = ''
 
     def create_device(self) -> None:
         """
@@ -26,6 +21,18 @@ class Register(object):
         self.gaid = str(uuid.uuid4())
         self.android_id = secrets.token_hex(8)
         self.device_id = base64.b64encode(secrets.token_bytes(32))
+        self.fingerprint = utils.crypto.CryptoUtils.build_fingerprint(self.device_id.decode())
+
+
+class Register(object):
+    def __init__(self):
+        self.session: Session = Session()
+
+    def register(self) -> None:
+        """
+        Registers a new device with the servers.
+        """
+        pass
 
     def get_keys(self) -> dict:
         """
